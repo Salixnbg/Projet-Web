2766cf2af5d2174fe7c38b467b37f076948ac5e6
Salix Nibigira <salixnibigira@gmail.com>
2025-12-09 15:20:33 +0100

    TD2

---
+++ Labo/TD2.js
@@ -0,0 +1,31 @@
+//Ex 1 
+
+var a = 10;
+var b = 5;
+var c = a+b;
+
+console.log(c)
+
+//Ex 2
+
+console.log(7+3);
+console.log(10+4);
+console.log(6*2);
+console.log(20/5);
+console.log(15%4);
+
+//Ex 3 
+
+if(15 > 10)
+    console.log("True");
+
+if(5 <= 5)
+    console.log("True");
+
+if(20 != 25)
+    console.log("True");
+
+var firstName ="Salix";
+var lastName ="Nibigira";
+
+console.log(firstName + " " + lastName);
