228647d884a3166ee4798765f9cf4a334b3864f1
Salix Nibigira <salixnibigira@gmail.com>
2025-12-09 14:09:48 +0100

    first commit

---
+++ Labo/TD1.js
@@ -0,0 +1,13 @@
+var chienAnimal = "Rex";
+var chatAnimal = "Pepper";
+console.log("Le nom de mon chien est:" + chienAnimal);
+console.log("Le nom de mon chat est:" + chatAnimal);
+
+var sonChat = "ronron";
+var sonChien = "ouaf";
+
+console.log(chienAnimal + " dit: " + sonChien);
+console.log(chatAnimal + " dit: " + sonChat);
+
+sonChat = "miaou";
+console.log(chatAnimal + " dit maintenant: " + sonChat);
