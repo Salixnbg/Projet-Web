754007ac639dcac2cefeb66757b291c17faa005a
Salix Nibigira <salixnibigira@gmail.com>
2025-12-10 11:07:28 +0100

    TD3

---
+++ Labo/TD3.js
@@ -0,0 +1,12 @@
+//Tâche 1
+
+var score = 8;
+
+    console.log("Compétences de niveauintermédiaire:", score > 0 && score < 10);
+
+//Tâche 2
+
+var timeRemaining = 0;
+var energy = 10;
+
+    console.log("Jeu terminé:", timeRemaining === 0 || energy === 0);
