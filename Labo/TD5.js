cb76cf128d7466adc7cb868467e8ce8c4fee15c3
Salix Nibigira <salixnibigira@gmail.com>
2025-12-15 12:21:09 +0100

    TD5

---
+++ Labo/TD5.js
@@ -0,0 +1,42 @@
+//Tache 1
+
+for(var num = 1; num <= 5; ++num){
+    console.log(num);
+}
+console.log("Counting completed");
+
+//Tache 2
+
+for(var num = 5; num > 0; --num){
+    console.log(num);
+}
+console.log("Countdown finished");
+
+
+//Tache 3
+
+var i = 1
+while (i <= 5){
+    console.log(i);
+    i++;  
+}
+console.log("Counting completed");
+
+
+//Tache 4
+
+var i = 5;
+do{
+    console.log(i);
+    i = i-1;
+}while(i > 0);
+console.log("Countdown finished");
+
+
+//Tache 5
+
+var i = 2018;
+while(i <= 2022){
+    console.log(i);
+    i++;
+}
